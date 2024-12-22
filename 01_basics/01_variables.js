63fad6d06bd994fe34c4182abf23f8d7302f3ed2
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-12-22 18:32:00 +0000

    variables added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1443
+let accountEmail = "ver@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+console.table([accountId , accountEmail, accountPassword, accountCity]);
